280ca31132f39114e4cc8ba10adc8a38620aba0b
sinohiro <sinohiro@momo-mail.com>
2021-07-31 18:10:42 +0900

    update home

--- my_nuxt_app/sinohiroHP/pages/components/home/Home.vue
+++ my_nuxt_app/sinohiroHP/pages/components/home/Home.vue
@@ -1,10 +1,73 @@
 <template>
   <section class="home">
     <HomeHero/>
+
+    <v-card
+      color="#333"
+      dark
+      tile
+    >
+      <v-container>
+        <v-row class="home-contents">
+          <v-col md="7">
+            <h3>Profile</h3>
+            <h2>Name</h2>
+            <p>sinohiro</p>
+            
+            <h2>School</h2>
+						<p>釧路工業高等専門学校<br>
+						National Institute of Technology, Kushiro College</p>
+            <v-btn
+              x-large
+              depressed
+              color="primary"
+              dark
+              href="https://www.kushiro-ct.ac.jp/"
+              target="_blank"
+            >
+              School HP
+            </v-btn>
+
+            <h2>Birthday</h2>
+							<p>01月18日 January 18</p>
+
+						<h2>My Hobby</h2>
+							<p>.Game<br>
+								.Watch anime<br>
+								.Making PC</p>
+          </v-col>
+          
+          <v-col md="3" class="home-contents-img">
+            <img src="../../../assets/img/sinohirosan.png">
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+
+    <v-card
+      color="white"
+      dark
+      tile
+    >
+      <v-container>
+        <v-row class="home-contents">
+          <v-col>
+            <h3>Contact</h3>
+            <div class="social">
+							<a href="https://www.facebook.com/profile.php?id=100025645427583" class="link facebook" target="_parent"><i class="fab fa-facebook-square"></i></a>
+							<a href="https://twitter.com/sinohiro966" class="link twitter" target="_parent"><i class="fab fa-twitter"></i></a>
+							<a href="https://github.com/sinohiro" class="link discord" target="_parent"><i class="fab fa-github-square"></i></a>
+						</div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </section>
 </template>
+
 <script>
 import HomeHero from './HomeHero.vue'
+import '@fortawesome/fontawesome-free/css/all.css'
  
 export default {
   components: {
@@ -14,6 +77,87 @@ export default {
 </script>
 <style lang="scss" scoped>
 .home {
-  padding-bottom: 50px;
+  padding-bottom: 35px;
 }
+
+h1 {
+  font-size: 60px;
+}
+
+h3 {
+  font-size: 30px;
+  color: hotpink;
+  border-bottom: 3px solid hotpink;
+}
+
+.hero_title {
+  padding: 5%;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.home-contents {
+  img {
+    width: 400px;
+	  height: 400px;
+    float: right;
+  }
+}
+
+// social button start--------------------------------------
+.social {
+  top: 50%;
+  text-align: center;
+	margin: 10px;
+}
+
+.social .link {
+  display: inline-block;
+  vertical-align: middle;
+  position: relative;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 2px dashed hotpink;
+  background-clip: content-box;
+  padding: 10px;
+  transition: .5s;
+  color: hotpink;
+  margin-left: 15px;
+  margin-right: 15px;
+  font-size: 70px;
+}
+
+.social .link span {
+  display: block;
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.social .link:hover {
+  padding: 20px;
+  color: hotpink;
+  margin-left: -5px;
+  transform: translateX(10px) rotate(360deg);
+}
+
+.social .link.discord {
+  background-color: #2C2F33;
+  color: white;
+}
+
+.social .link.twitter {
+  background-color: #00ACEE;
+  color: white;
+}
+
+.social .link.facebook {
+  background-color: #3B5998;
+  color: white;
+}
+// social button end--------------------------------------
 </style>
--- my_nuxt_app/sinohiroHP/pages/components/home/HomeHero.vue
+++ my_nuxt_app/sinohiroHP/pages/components/home/HomeHero.vue
@@ -1,26 +1,50 @@
 <template>
   <section class="home-hero">
-    <v-container fluid fill-height class="home-hero__content">
-      <v-row>
-        <v-col class="home-hero__content-text">
-          <p>Welcome to</p>
-          <p>sinohiro server</p>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+      fluid
+      fill-height
+    >     
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        :slide="slides"
+      >
+          <v-row
+            class="fill-height"
+            align="start"
+            justify="start"
+          >
+            <div class="hero_title">
+              <h1>{{ item.title }}</h1>
+              <p></p>
+              <p>{{ item.messege }}</p>
+            </div>
+          </v-row>
+      </v-carousel-item>
+    </v-carousel>
   </section>
 </template>
+
 <style  lang="scss" scoped>
 
-.home-hero__content {
-  background: url("../../../assets/img/bg.png");
-  background-size: cover;
-  background-position: center top;
-  width: 100%;
-  height: 50vh;
+h1 {
+  font-size: 60px;
+}
 
+.hero_title {
+  padding: 5%;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.home-hero {
   @include display_pc {
-    height: 100vh;
   }
  
   &-text {
@@ -36,3 +60,34 @@
   }
 }
 </style>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            title: 'Minecraft',
+            messege: 'Minecraftサーバのマップ情報をリアルタイムで確認できます.'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            title: 'Entertainment Cloud',
+            messege: 'アカウント登録することで10Gbのクラウドストレージを使用することができます.'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            title: 'Game',
+            messege: 'WebGLで動く簡単なゲームを遊ぶことができます.'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            title: 'SpeedTest',
+            messege: 'sinohiro serverとの接続テストを行うことができます.'
+          },
+        ],
+      }
+    },
+  }
+</script>
