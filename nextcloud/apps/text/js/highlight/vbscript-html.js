5e3c69b3ae83452910fd5750c55ea5dfabf720cd
sinohiro <sinohiro@momo-mail.com>
2021-09-25 18:01:32 +0900

    firstcommit

---
+++ nextcloud/apps/text/js/highlight/vbscript-html.js
@@ -0,0 +1,2 @@
+(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[182],{620:function(n,t,e){"use strict";n.exports=function(n){return{name:"VBScript in HTML",subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=vbscript-html.js.map?v=2d20c6d9a6e432a6f5b0
---
+++ nextcloud/apps/viewer/l10n/vi.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "viewer",
+    {
+    "Your browser does not support audio." : "Trình duyệt của bạn không hỗ trợ âm thanh.",
+    "Error loading {name}" : "Lỗi khi tải {name}",
+    "Your browser does not support videos." : "Trình duyệt của bạn không hỗ trợ video.",
+    "Open sidebar" : "Mở thanh bên",
+    "Delete" : "Xóa",
+    "View" : "Xem"
+},
+"nplurals=1; plural=0;");
